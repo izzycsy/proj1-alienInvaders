ea01a4e37876e55e698312b915d8bbfb181783fa
izzycsy <izzycsy@gmail.com>
2020-06-26 18:36:17 +0800

    commit1

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 console.log("hello script js");
 
-//mainCanvas
+//mainCanvas 
 var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
 var x = 50;
 var y = 50;
-var moonRadius = 20;
+var radiusMoon = 20;
 var dx = 2;
 var dy = 0;
 
-//Duplicate moon
+//Rows & Columns of moonBugs
 var rowMoon = 5;
 var columnMoon = 11;
 var paddingMoon = 10;
@@ -24,34 +24,37 @@ for (var c = 0; c < columnMoon; c++) {
             x: 0,
             y: 0,
             state: 1
-        } //each moonBug has a x,y coordinate & alive / dead state
-    }
+        }; //each moonBug has a x,y coordinate & alive / dead state
+    };
 }
 
 function drawMoon() {
     for (var c = 0; c < columnMoon; c++) {
         for (var r = 0; r < rowMoon; r++) {
-            var moonBugsX = (c * paddingMoon) + offsetLeftMoon;
+            var moonBugsX = (c * (radiusMoon + paddingMoon)) + offsetLeftMoon;
+            var moonBugsY = (c * (radiusMoon + paddingMoon))+ offsetTopMoon;
 
             moonBugs[c][r].x = moonBugsX;
+            moonBugs[c][r].y = moonBugsY;
 
             context.beginPath();
-            context.arc(x, y, moonRadius, 0, 2 * Math.PI);
+            context.arc(x, y, radiusMoon, 0, 2 * Math.PI);
             context.fillStyle = "#e6e6e6";
             context.fill();
             context.closePath();
-        }
-    }
+        };
+    };
 }
 
 // Moon move sideways
 function move() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawMoon();
-    if (x + dx > canvas.width - moonRadius || x + dx < moonRadius) {
+    if (x + dx > canvas.width - radiusMoon || x + dx < radiusMoon) {
         dx = -dx;
     }
     x += dx;
 }
 
-// setInterval(move, 20); //frame rate: 0.02s
+setInterval(move, 20); //frame rate: 0.02s
+
